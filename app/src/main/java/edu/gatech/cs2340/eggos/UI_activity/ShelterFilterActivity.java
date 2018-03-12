8a3b4d9410b241e90dcdbcd27f4d0bfec53327f2
Vibhav <vibhavb00@gmail.com>
2018-03-12 17:26:52 -0400

    Added UI for shelter screen

---
+++ app/src/main/java/edu/gatech/cs2340/eggos/UI_activity/ShelterFilterActivity.java
@@ -0,0 +1,24 @@
+package edu.gatech.cs2340.eggos.UI_activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import edu.gatech.cs2340.eggos.Model.User.UserHolder;
+import edu.gatech.cs2340.eggos.R;
+
+/**
+ * Created by Sheetal on 3/12/2018.
+ */
+
+public class ShelterFilterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelter_filter);
+    }
+}
