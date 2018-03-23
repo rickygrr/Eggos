62cf1675f5c0a4e9fb02ecf4370c70b940f1d3a2
Woradorn K <chateau86@gmail.com>
2018-03-23 15:14:37 -0400

    Firebase-based login implemented.
    Signed-off-by: Woradorn K <chateau86@gmail.com>

--- app/src/main/java/edu/gatech/cs2340/eggos/UI_activity/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/eggos/UI_activity/LoginActivity.java
@@ -1,37 +1,27 @@
 package edu.gatech.cs2340.eggos.UI_activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import edu.gatech.cs2340.eggos.Model.User.UserHolder;
 import edu.gatech.cs2340.eggos.R;
@@ -47,12 +37,16 @@ public class LoginActivity extends AppCompatActivity{
     private EditText mUserView;
     private EditText mPasswordView;
     private View mLoginFormView;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
         mUserView = (EditText) findViewById(R.id.username);
         mPasswordView = (EditText) findViewById(R.id.password);
 
@@ -109,11 +103,11 @@ public class LoginActivity extends AppCompatActivity{
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        /*if (TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_incorrect_password));
             focusView = mPasswordView;
             cancel = true;
-        }
+        }*/
 
         if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
@@ -141,11 +135,11 @@ public class LoginActivity extends AppCompatActivity{
             cancel = true;
         }
 
-        if (!TextUtils.isEmpty(user) && !isPasswordValid(password)) {
+        /*if (!TextUtils.isEmpty(user) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_incorrect_password));
             focusView = mPasswordView;
             cancel = true;
-        }
+        }*/
         // Check for a valid username.
         if (TextUtils.isEmpty(user)) {
             mUserView.setError(getString(R.string.error_field_required));
@@ -153,14 +147,7 @@ public class LoginActivity extends AppCompatActivity{
             cancel = true;
 
         }
-        
 
-        //now do login
-        if (!UserHolder.getInstance().login(user,password)){
-            mPasswordView.setError(getString(R.string.error_incorrect_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
@@ -168,11 +155,27 @@ public class LoginActivity extends AppCompatActivity{
                 focusView.requestFocus();
             }
         } else {
-            // go to main screen
-            Context context = mLoginFormView.getContext();
-            Intent intent = new Intent(context, DummyAppActivity.class);
-            context.startActivity(intent);
-            finish();
+            mAuth.signInWithEmailAndPassword(user, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LoginActivity", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Context context = mLoginFormView.getContext();
+                            Intent intent = new Intent(context, DummyAppActivity.class);
+                            context.startActivity(intent);
+                            finish();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("LoginActivity", "signInWithEmail:failure", task.getException());
+                            //updateUI(null);
+                            mPasswordView.setError(getString(R.string.error_incorrect_password));
+                        }
+                    }
+                 });
         }
     }
 
