bb57a191708a9911d3d31f6a6c65c802211fc24f
Dawar Ahmed <dahmedjetpack@gmail.com>
2018-04-15 17:20:37 -0400

    Dawar's unit tessts

--- app/src/test/java/edu/gatech/cs2340/eggos/ExampleUnitTest.java
+++ app/src/test/java/edu/gatech/cs2340/eggos/ExampleUnitTest.java
@@ -1,17 +1,52 @@
 package edu.gatech.cs2340.eggos;
 
+import org.junit.Test;
+import edu.gatech.cs2340.eggos.Model.Shelter.Shelter;
+import static org.junit.Assert.*;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void freeRoom_NegativeCap() throws Exception {
+        int capmax = 100;
+        int capacity = 20;
+        int cap = -5;
+        Shelter testShelter = new Shelter(0, "test", 0, 0, 0, 0, "", 0, 0, "", "");
+        testShelter._Capacity_max = capmax;
+        testShelter._Capacity_current = capacity;
+        testShelter.freeRoom(cap);
+        assertEquals(capacity, testShelter._Capacity_current);
     }
+
+    @Test
+    public void freeRoom_BigCap() throws Exception {
+        int capmax = 100;
+        int capacity = 20;
+        int cap = 100;
+        Shelter testShelter = new Shelter(0, "test", 0, 0, 0, 0, "", 0, 0, "", "");
+        testShelter._Capacity_current = capacity;
+        testShelter._Capacity_max = capmax;
+        testShelter.freeRoom(cap);
+        assertEquals(capacity, testShelter._Capacity_current);
+    }
+
+    @Test
+    public void freeRoom_Normal() throws Exception {
+        int capmax = 100;
+        int capacity = 20;
+        int cap = 50;
+        Shelter testShelter = new Shelter(0, "test", 0, 0, 0, 0, "", 0, 0, "", "");
+        testShelter._Capacity_max = capmax;
+        testShelter._Capacity_current = capacity;
+        testShelter.freeRoom(cap);
+        assertEquals(capacity + cap, testShelter._Capacity_current);
+    }
+
+
 }
